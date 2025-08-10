21a84af52055f074ba1ca27119624f3b19110113
MtzAna26 <anamaria.cat.2606@gmail.com>
2025-08-10 13:47:34 -0600

    Agregar carpeta 01_hola_java con el 1er ejercicio

---
+++ hola_java/HolaJava.java
@@ -0,0 +1,15 @@
+package hola_java;
+public class HolaJava {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+        System.out.println("Lassie, Palomo y Cat te saludan 🐶🐶🐱");
+    }
+}
+
+/*
+ * Para correr el 1er programa en Java:
+ * 1. Guardar el archivo como HolaJava.java (el nombre debe coincidir con el de la clase pública)
+ * 2. Abre una terminal en la carpeta donde guardaste el archivo.
+ * 3. Compila el programa 
+ * javac HolaJava.java 
+ */
